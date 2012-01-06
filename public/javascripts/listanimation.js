200357124838ca8e01f74acfc239123025d601a1
ksk-uchi <math_55jp@yahoo.co.jp>
2012-01-06 19:36:36 +0900

    corresponded when cannot gets cloth object.

--- public/javascripts/listanimation.js
+++ public/javascripts/listanimation.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     var selector = {
         photo   : '.list',
         obi     : '#list',
@@ -11,35 +11,40 @@ $(function(){
     var pos = 0;
     var slideWide = $(selector.visible).innerWidth();
     var num = $(selector.photo).length;
-    var photoWidth = $(selector.photo).outerWidth({margin:true});
+    if (num === 0) {
+        $(selector.left).css('display', 'none');
+        $(selector.right).css('display', 'none');
+    } else {
+        var photoWidth = $(selector.photo).outerWidth({margin:true});
 
-    $(selector.obi).css('width', (num * photoWidth) + 'px');
+        $(selector.obi).css('width', (num * photoWidth) + 'px');
 
-    // 右側がクリックされたらulを左にずらす
-    $(selector.right).click(function(){
-        if(pos <= slideWide * -1 * (num / 5 - 1)){
-            pos = pos;
-        }else{
-            pos -= slideWide;
-            $(selector.obi).animate({'left': pos + 'px'}, 'normal', 'swing');
-        }
-    })
+        // 右側がクリックされたらulを左にずらす
+        $(selector.right).click(function(){
+            if (pos <= slideWide * -1 * (num / 5 - 1)) {
+                pos = pos;
+            } else {
+                pos -= slideWide;
+                $(selector.obi).animate({'left': pos + 'px'}, 'normal', 'swing');
+            }
+        })
 
-    // 左側がクリックされたらulを右にずらす
-    $(selector.left).click(function(){
-        if(pos >= 0){
-            pos = pos;
-        }else{
-            pos += slideWide;
-            $(selector.obi).animate({'left': pos + 'px'}, 'normal', 'swing');
-        }
-    })
+        // 左側がクリックされたらulを右にずらす
+        $(selector.left).click(function(){
+            if (pos >= 0) {
+                pos = pos;
+            } else {
+                pos += slideWide;
+                $(selector.obi).animate({'left': pos + 'px'}, 'normal', 'swing');
+            }
+        })
 
-    // liがクリックされたら内容をdivに書き込み、pにidを付与する
-    $(selector.photo).click(function(){
-        var im = $(this)[0].childNodes[0].childNodes[0].src,
-            alt = $(this)[0].childNodes[0].childNodes[0].alt;
-        $(selector.screen).children()[0].childNodes[0].src = im;
-        $(selector.info).html(alt);
-    })
+        // liがクリックされたら内容をdivに書き込み、pにidを付与する
+        $(selector.photo).click(function(){
+            var im = $(this)[0].childNodes[0].childNodes[0].src,
+                alt = $(this)[0].childNodes[0].childNodes[0].alt;
+            $(selector.screen).children()[0].childNodes[0].src = im;
+            $(selector.info).html(alt);
+        })
+    }
 });
--- routes/test.js
+++ routes/test.js
@@ -10,8 +10,8 @@ var arr = {
 module.exports = arr;
 */
 module.exports = {
-    photos : (function (){
-        var arr = [{name:'/images/img_01.png', data:'hoge'},
+    photos : (function () {
+/*        var arr = [{name:'/images/img_01.png', data:'hoge'},
                    {name:'/images/img_00.png', data:'fuga'},
                    {name:'/images/img_02.png', data:'mofu'},
                    {name:'/images/img_03.png', data:'moga'},
@@ -22,7 +22,9 @@ module.exports = {
                    {name:'/images/img_02.png', data:'moge'},
                    {name:'/images/img_03.png', data:'hoge'},
                    {name:'/images/img_04.png', data:'hage'},
-                   {name:'/images/img_05.png', data:'fuga'}]
+                   {name:'/images/img_05.png', data:'fuga'}];
+*/
+        var arr = [];
         return arr;
     })()
 };
