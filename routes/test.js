087b3c372b1613e22667e4c797ac17860b9d1113
ksk-uchi <math_55jp@yahoo.co.jp>
2012-01-06 14:02:54 +0900

    repair first views photo, data in bigscreen.

--- routes/test.js
+++ routes/test.js
@@ -11,8 +11,8 @@ module.exports = arr;
 */
 module.exports = {
     photos : (function (){
-        var arr = [{name:'/images/img_00.png', data:'hoge'},
-                   {name:'/images/img_01.png', data:'fuga'},
+        var arr = [{name:'/images/img_01.png', data:'hoge'},
+                   {name:'/images/img_00.png', data:'fuga'},
                    {name:'/images/img_02.png', data:'mofu'},
                    {name:'/images/img_03.png', data:'moga'},
                    {name:'/images/img_04.png', data:'moge'},
